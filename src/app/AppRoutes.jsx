7548a60f7a5f0b31ee87a265fc6e6a74b7cbdf75
vicencoo <vicencollanaj2004@gmail.com>
2026-06-24 17:25:05 +0200

    fixing delay

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -1,54 +1,91 @@
-import { Suspense, lazy } from "react";
+// import { Suspense, lazy } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import { Layout } from "../layout/Layout";
+// import { HomePage } from "../pages/home/Home";
+
+// // const HomePage = lazy(() =>
+// //   import("../pages/home").then((module) => ({ default: module.HomePage })),
+// // );
+// const Residence = lazy(() =>
+//   import("../pages/residence").then((module) => ({
+//     default: module.Residence,
+//   })),
+// );
+// const Apartments = lazy(() =>
+//   import("../pages/apartments").then((module) => ({
+//     default: module.Apartments,
+//   })),
+// );
+// const ViewApartment = lazy(() =>
+//   import("../pages/viewApartment").then((module) => ({
+//     default: module.ViewApartment,
+//   })),
+// );
+// const Parking = lazy(() =>
+//   import("../pages/parking").then((module) => ({ default: module.Parking })),
+// );
+// const Villas = lazy(() =>
+//   import("../pages/villas").then((module) => ({ default: module.Villas })),
+// );
+// const ViewVilla = lazy(() =>
+//   import("../pages/viewVilla").then((module) => ({
+//     default: module.ViewVilla,
+//   })),
+// );
+// const Contact = lazy(() =>
+//   import("../pages/contact").then((module) => ({ default: module.Contact })),
+// );
+
+// export const AppRoutes = () => {
+//   return (
+//     <Suspense fallback={null}>
+//       <Routes>
+//         <Route element={<Layout />}>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/residence" element={<Residence />} />
+//           <Route path="/apartments" element={<Apartments />} />
+//           <Route
+//             path="/apartments/:stairSlug/:unitSlug"
+//             element={<ViewApartment />}
+//           />
+//           <Route path="/parking" element={<Parking />} />
+//           <Route path="/villas" element={<Villas />} />
+//           <Route path="/villas/:id" element={<ViewVilla />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Route>
+//       </Routes>
+//     </Suspense>
+//   );
+// };
+
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 
-const HomePage = lazy(() =>
-  import("../pages/home").then((module) => ({ default: module.HomePage })),
-);
-const Residence = lazy(() =>
-  import("../pages/residence").then((module) => ({ default: module.Residence })),
-);
-const Apartments = lazy(() =>
-  import("../pages/apartments").then((module) => ({
-    default: module.Apartments,
-  })),
-);
-const ViewApartment = lazy(() =>
-  import("../pages/viewApartment").then((module) => ({
-    default: module.ViewApartment,
-  })),
-);
-const Parking = lazy(() =>
-  import("../pages/parking").then((module) => ({ default: module.Parking })),
-);
-const Villas = lazy(() =>
-  import("../pages/villas").then((module) => ({ default: module.Villas })),
-);
-const ViewVilla = lazy(() =>
-  import("../pages/viewVilla").then((module) => ({ default: module.ViewVilla })),
-);
-const Contact = lazy(() =>
-  import("../pages/contact").then((module) => ({ default: module.Contact })),
-);
+import { HomePage } from "../pages/home";
+import { Residence } from "../pages/residence";
+import { Apartments } from "../pages/apartments";
+import { ViewApartment } from "../pages/viewApartment";
+import { Parking } from "../pages/parking";
+import { Villas } from "../pages/villas";
+import { ViewVilla } from "../pages/viewVilla";
+import { Contact } from "../pages/contact";
 
 export const AppRoutes = () => {
   return (
-    <Suspense fallback={null}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/residence" element={<Residence />} />
-          <Route path="/apartments" element={<Apartments />} />
-          <Route
-            path="/apartments/:stairSlug/:unitSlug"
-            element={<ViewApartment />}
-          />
-          <Route path="/parking" element={<Parking />} />
-          <Route path="/villas" element={<Villas />} />
-          <Route path="/villas/:id" element={<ViewVilla />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/residence" element={<Residence />} />
+        <Route path="/apartments" element={<Apartments />} />
+        <Route
+          path="/apartments/:stairSlug/:unitSlug"
+          element={<ViewApartment />}
+        />
+        <Route path="/parking" element={<Parking />} />
+        <Route path="/villas" element={<Villas />} />
+        <Route path="/villas/:id" element={<ViewVilla />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 };
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,64 +3,63 @@ import { initReactI18next } from "react-i18next";
 
 import sqCommon from "./locales/sq/common.json";
 import enCommon from "./locales/en/common.json";
-import sqHome from "./locales/sq/home.json";
-import enHome from "./locales/en/home.json";
-import sqResidence from "./locales/sq/residence.json";
-import enResidence from "./locales/en/residence.json";
-import sqVillas from "./locales/sq/villas.json";
-import enVillas from "./locales/en/villas.json";
-import sqViewVilla from "./locales/sq/viewVilla.json";
-import enViewVilla from "./locales/en/viewVilla.json";
-import sqApartments from "./locales/sq/apartments.json";
-import enApartments from "./locales/en/apartments.json";
-import sqViewApartment from "./locales/sq/viewApartment.json";
-import enViewApartment from "./locales/en/viewApartment.json";
-import sqParking from "./locales/sq/parking.json";
-import enParking from "./locales/en/parking.json";
-import sqContact from "./locales/sq/contact.json";
-import enContact from "./locales/en/contact.json";
+
+const namespaceLoaders = {
+  sq: {
+    home: () => import("./locales/sq/home.json"),
+    residence: () => import("./locales/sq/residence.json"),
+    villas: () => import("./locales/sq/villas.json"),
+    viewVilla: () => import("./locales/sq/viewVilla.json"),
+    apartments: () => import("./locales/sq/apartments.json"),
+    viewApartment: () => import("./locales/sq/viewApartment.json"),
+    parking: () => import("./locales/sq/parking.json"),
+    contact: () => import("./locales/sq/contact.json"),
+  },
+  en: {
+    home: () => import("./locales/en/home.json"),
+    residence: () => import("./locales/en/residence.json"),
+    villas: () => import("./locales/en/villas.json"),
+    viewVilla: () => import("./locales/en/viewVilla.json"),
+    apartments: () => import("./locales/en/apartments.json"),
+    viewApartment: () => import("./locales/en/viewApartment.json"),
+    parking: () => import("./locales/en/parking.json"),
+    contact: () => import("./locales/en/contact.json"),
+  },
+};
+
+const lazyLocaleBackend = {
+  type: "backend",
+  read(language, namespace, callback) {
+    const loader = namespaceLoaders[language]?.[namespace];
+
+    if (!loader) {
+      callback(new Error(`Missing locale namespace: ${language}/${namespace}`));
+      return;
+    }
+
+    loader()
+      .then((module) => callback(null, module.default ?? module))
+      .catch((error) => callback(error));
+  },
+};
 
 const savedLanguage = localStorage.getItem("language") || "sq";
 
-i18n.use(initReactI18next).init({
+i18n.use(lazyLocaleBackend).use(initReactI18next).init({
   resources: {
     sq: {
       common: sqCommon,
-      home: sqHome,
-      residence: sqResidence,
-      villas: sqVillas,
-      viewVilla: sqViewVilla,
-      apartments: sqApartments,
-      viewApartment: sqViewApartment,
-      parking: sqParking,
-      contact: sqContact,
     },
     en: {
       common: enCommon,
-      home: enHome,
-      residence: enResidence,
-      villas: enVillas,
-      viewVilla: enViewVilla,
-      apartments: enApartments,
-      viewApartment: enViewApartment,
-      parking: enParking,
-      contact: enContact,
     },
   },
   lng: savedLanguage,
   fallbackLng: "sq",
-  ns: [
-    "common",
-    "home",
-    "residence",
-    "villas",
-    "viewVilla",
-    "apartments",
-    "viewApartment",
-    "parking",
-    "contact",
-  ],
+  ns: ["common"],
   defaultNS: "common",
+  fallbackNS: "common",
+  partialBundledLanguages: true,
   interpolation: {
     escapeValue: false,
   },
